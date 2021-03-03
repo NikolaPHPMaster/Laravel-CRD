beda92231f7bb548dd45e3b70645fb85fa87f8bc
NIKOLA MARKOVIC <pomocnih@gmail.com>
2021-03-03 14:52:34 +0100

    first commit

---
+++ resources/views/nvb/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active">
+<a class="nav-link" href="/">Pocetna</a>
+</li>
+<li class="nav-item active">
+<a class ="nav-link" href="/servisi">Servisi</a>
+</li>
+<li class="nav-item active">
+<a class="nav-link" href="/onama">O nama</a>
+</li>
+<li class="nav-item active">
+<a class="nav-link" href="/strposts">Postovi</a>
+</li>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+    <li class="nav-item active">
+    <a class="nav-link" href="/strposts/create">Kreiraj Objavu</a>
+    </li>
+</ul>
+</nav>
